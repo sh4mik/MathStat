ebed659bca3ffdc6ae34524a465d8dbdd402d885
Shamil Mustafaev <sh.mustafaev@mail.ru>
2020-09-22 22:01:16 +0300

    code and report on squared omega criteria

--- CourseWork/course_nw.py
+++ CourseWork/course_nw.py
@@ -2,6 +2,7 @@ from scipy import integrate
 import numpy as np
 from math import *
 from numpy import inf
+import matplotlib.pyplot as plt
 
 
 def check_normal(data):
@@ -21,6 +22,7 @@ def check_normal(data):
     return sum
 
 
+bins = 20
 num = [10, 100, 500]
 if __name__ == '__main__':
     for size in num:
@@ -28,8 +30,12 @@ if __name__ == '__main__':
         sum = check_normal(array)
         print(np.around(sum, decimals=2))
 
-
     #check flexibility
     for size in num:
         array_u = np.random.uniform(-np.sqrt(3), np.sqrt(3), size)
+        if size == 100:
+            n, bins, patches = plt.hist(array_u, bins, density=1, facecolor='grey', edgecolor='black', alpha=0.2)
+            plt.title(r'Uniform Distribution, N=%i' % size)
+            plt.savefig("Uniform, 100" + ".png", format='png')
+            plt.show()
         print('Uniform ', size, ': ', np.around(check_normal(array_u), decimals=2))
